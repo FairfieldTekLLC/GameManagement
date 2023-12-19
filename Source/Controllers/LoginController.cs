ec28c6c64534d5c9f884227a5695d4a155807e56
Chaple <vince@fairfieldtek.com>
2023-12-19 08:13:13 -0500

    Initial commit

---
+++ Source/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using GameManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameManager.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult Login(string username, string password)
+        {
+            using (var con = new GameManagementContext())
+            {
+                var player = con.Players.FirstOrDefault(x => x.Username == username);
+                if (player == null)
+                {
+                    return Json(new { auth = false });
+                }
+
+                if (player.Password == password)
+                {
+
+                    PlayerSessionKey psk = new PlayerSessionKey()
+                    {
+                        FkPlayerId = player.PkPlayerId,
+                        IssueDt = DateTime.Now,
+                        SessionKey = Guid.NewGuid()
+                    };
+                    con.PlayerSessionKeys.Add(psk);
+                    con.SaveChanges();
+
+
+
+                    return Json(new { auth = true, sessionKey = psk.SessionKey });
+                }
+
+            }
+            return Json(new { auth = false });
+        }
+    }
+}
